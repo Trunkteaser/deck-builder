28d1ba7296590bd4dc14504207a70772e298cbd2
BillyBonka <josefmhenriksson@gmail.com>
2026-01-30 13:23:15 +0100

    Daddy

--- enemies/banshee/banshee_attack.gd
+++ enemies/banshee/banshee_attack.gd
@@ -9,16 +9,7 @@ func perform_action() -> void:
 
 	#var target_array: Array[Node] = [target]
 	var modified_damage = enemy.modifier_handler.get_modified_value(damage, Modifier.Type.DMG_DEALT)
-	#
-	#var tween := create_tween()
-	#var start := enemy.global_position
-	#var end := target.global_position + Vector2.RIGHT * 64
-	#tween.set_trans(Tween.TRANS_QUINT)
-	#tween.tween_property(enemy, "global_position", end, 0.2)
-	#tween.tween_callback(damage_effect.apply.bind(target_array, damage))
-	#tween.tween_interval(0.25)
-	#tween.tween_property(enemy, "global_position", start, 0.4)
-	#tween.finished.connect(func(): Events.enemy_action_completed.emit(enemy))
+
 	
 	var tween := create_tween()
 	var start := enemy.global_position
