45b7cc420865fd7ae30000b6bbc9c73236580fb3
Trunkteaser <simonhayrapetian@gmail.com>
2026-02-03 22:34:39 +0100

    Inspiration reward refactor.
    Inspiration reward is now set by a single number instead of 2. 20% up and down from average possible.

--- resources/battle_stats.gd
+++ resources/battle_stats.gd
@@ -5,11 +5,12 @@ class_name BattleStats
 enum Tier {EASY, NORMAL, ELITE, BOSS}
 @export var tier: Tier
 @export_range(0.0, 10.0) var weight: float = 1
-@export var insp_reward_min: int
-@export var insp_reward_max: int
+@export var avg_insp_reward: int
 @export var enemies: PackedScene
 
 var accumulated_weight: float = 0.0
 
 func roll_inspiration_reward() -> int:
-	return randi_range(insp_reward_min, insp_reward_max)
+	var min_reward: int = floori(avg_insp_reward*0.8)
+	var max_reward: int = floori(avg_insp_reward*1.2)
+	return randi_range(min_reward, max_reward)
--- scenes/ui/mana_icon.gd
+++ scenes/ui/mana_icon.gd
@@ -4,7 +4,7 @@ class_name ManaIcon
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var texture_rect: TextureRect = $TextureRect
 @onready var mana_animation: AnimatedSprite2D = $ManaAnimation
-var last_animation: String
+#var last_animation: String
 
 func _ready() -> void:
 	await get_tree().process_frame
@@ -12,19 +12,21 @@ func _ready() -> void:
 	#mana_animation.play("fill")
 	#last_animation = "fill"
 
-func idle() -> void:
-	mana_animation.play("idle")
-	last_animation = "idle"
-
-func change_icon(icon: Texture2D) -> void:
-	texture_rect.texture = icon
-
-func _on_mana_animation_animation_finished() -> void:
-	if last_animation != "empty":
-		idle()
-#
 func empty() -> void:
 	sprite_2d.show()
 	mana_animation.hide()
 	#mana_animation.play("empty")
 	#last_animation = "empty"
+
+func change_icon(icon: Texture2D) -> void:
+	texture_rect.texture = icon
+
+#func idle() -> void:
+	#mana_animation.play("idle")
+	#last_animation = "idle"
+
+
+
+#func _on_mana_animation_animation_finished() -> void:
+	#if last_animation != "empty":
+		#idle()
