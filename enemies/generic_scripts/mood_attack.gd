5f5143d70e77d9a1e92f57958e7b14d074d0a4e8
Trunkteaser <simonhayrapetian@gmail.com>
2026-02-06 21:47:58 +0100

    Spider bro done.
    Spider bro is done, and web attack is using a new generic script for attacks that apply a mood.

---
+++ enemies/generic_scripts/mood_attack.gd
@@ -0,0 +1,26 @@
+extends ChanceBasedAction
+
+@export var damage := 10
+@export var mood: Mood
+@export var stacks := 1
+
+func perform_action() -> void:
+	var modified_damage := enemy.modifier_handler.get_modified_value(damage, Modifier.Type.DMG_DEALT)
+
+	var tween := create_tween()
+	var start := enemy.global_position
+	var end := enemy.global_position + Vector2.LEFT * 64
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(enemy, "global_position", end, 0.1)
+	tween.tween_callback(Apply.damage.bind(targets, modified_damage))
+	tween.tween_callback(Apply.mood.bind(targets, mood, stacks))
+	tween.tween_callback(SFXPlayer.play.bind(sfx))
+	tween.tween_interval(0.05)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(enemy, "global_position", start, 0.1)
+	tween.finished.connect(func(): Events.enemy_action_completed.emit(enemy))
+
+func update_intent_text() -> void:
+	var modified_dmg := enemy.modifier_handler.get_modified_value(damage, Modifier.Type.DMG_DEALT)
+	modified_dmg = hero.modifier_handler.get_modified_value(modified_dmg, Modifier.Type.DMG_TAKEN)
+	intent.current_text = intent.base_text % modified_dmg
