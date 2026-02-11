ef6cb1a6c8258ce05e64dd2d6cb06dbfbb8e6664
BillyBonka <josefmhenriksson@gmail.com>
2026-02-11 12:36:31 +0100

    all should  fear me

---
+++ mantras/all_should_fear_me.gd
@@ -0,0 +1,22 @@
+extends Mantra
+
+const FEAR = preload("uid://t28wluftr7m")
+
+func initialize_mantra(_mantra_ui: MantraUI) -> void:
+	print("This happens when mantra is obtained.")
+	print("Can connect to signals.")
+
+func activate_mantra(mantra_ui: MantraUI) -> void:
+	#var hero := mantra_ui.get_tree().get_nodes_in_group("hero")
+	var enemies := mantra_ui.get_tree().get_nodes_in_group("enemies")
+	Apply.mood(enemies, FEAR, 1)
+	print("This happens at specific times based on type.")
+	mantra_ui.flash()
+
+func deactivate_mantra(_mantra_ui: MantraUI) -> void:
+	print("This happens when mantra is lost.")
+	print("EventBased should disconnect from signals.")
+
+func get_tooltip() -> String:
+	return tooltip
+	# Only useful if "magic number".
